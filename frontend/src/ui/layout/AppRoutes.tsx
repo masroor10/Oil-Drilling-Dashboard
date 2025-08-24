74d235dd0d4f5eb8b5cdb02889618402892612ca
Masroor Ahmed <masroor.ahmed@devsatmelior.com>
2025-08-24 06:08:29 +0500

    updated Approute lazy loading

--- frontend/src/ui/layout/AppRoutes.tsx
+++ frontend/src/ui/layout/AppRoutes.tsx
@@ -1,15 +1,18 @@
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-
-import Dashboard from '../pages/Dashboard';
 import { Layout } from '.';
+
+const Dashboard = lazy(() => import('../pages/Dashboard'));
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path='/' element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
