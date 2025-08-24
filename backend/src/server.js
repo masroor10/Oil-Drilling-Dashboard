939511b74852b40c0a9df06b5c9d6754716b633d
Masroor Ahmed <masroor.ahmed@devsatmelior.com>
2025-08-24 06:46:20 +0500

    updated path for frontend in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,10 +21,10 @@ app.use('/api/chatbot', chatBotRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
